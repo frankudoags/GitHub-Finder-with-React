0cb9b1cab09e65903ae7bc38831f599a42f170ac
Udoagwa Franklin <54338168+frankudoags@users.noreply.github.com>
2022-08-11 10:21:29 +0100

    Update AlertContext.jsx

--- src/context/alert/AlertContext.jsx
+++ src/context/alert/AlertContext.jsx
@@ -14,7 +14,7 @@ export const AlertProvider = ({children}) => {
 
 
         //Function to Set the alert state which takes in the message and type of alert
-        //t also has a setTimeout to clear the alert after 5 secs
+        //t also has a setTimeout to clear the alert after 3 secs
         const setAlert = (msg, type) => {
             dispatch({
                 type: 'SET_ALERT',
